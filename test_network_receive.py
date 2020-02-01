36d2feca929d91875a2fffc91130c3c6c767a4a9
KhanSimeoni <simeonikh@gmail.com>
2020-01-31 19:05:01 -0500

    This code has RAM!
    This code can remember the location of one ball. You can adjust the "smoothness" of your data by increasing `smoothLevel`, but turn it too high and it wont notice if a ball leaves view for a while. I found 10 was a good number, but that number may change once I add compatibility with multiple balls

--- test_network_receive.py
+++ test_network_receive.py
@@ -15,11 +15,45 @@ if len(sys.argv) != 2:
 
 ip = sys.argv[1]
 
+smoothLevel = 10
+
 NetworkTables.initialize(server=ip)
-table = NetworkTables.getTable("ROS")
-
+table = NetworkTables.getTable("ML")
+i = 0
+ball1memX = 0
 while 1:
-    alive = table.getNumber('alive', '-1')
-    print("received " + str(alive) + " from networktables")
+    object1posX = table.getNumberArray('boxes', [0,0,0,0])
+    numBall = table.getNumber('nb_objects', 0)
+    try:    
+        ball1centerX = ((object1posX[2]-object1posX[0])/2 + object1posX[0])/5.33
+    except:
+        pass
+    if (numBall == 1):
+        try:     
+            ball1memX = ball1centerX 
+            i = 0
+        except:
+            pass
 
-    time.sleep(1)
+    if (i >= smoothLevel):
+        ball1memX = 0
+    else:
+        pass
+
+    if (numBall == 1):
+        try:     
+            print("The center of ball 1 is at " + str(ball1memX))
+        except:
+            print("not enough ballage")
+    elif (numBall == 2):
+        print("The center of ball 1 is at " + str(((object1posX[2]-object1posX[0])/2 + object1posX[0]/5.33)))
+        print("The center of ball 2 is at " + str(((object1posX[3]-object1posX[1])/2 + object1posX[1]/5.33)))
+    elif (numBall > 2):
+        print("Too many balls")
+    elif (numBall <= 0):
+        if (i <= 19):
+            print("The center of ball 1 is at " + str(ball1memX))
+        else:
+           print("not enough ballage")
+    i = i + 1
+    time.sleep(0.2)
--- test_network_send.py
+++ test_network_send.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env python
+# from robotpy.readthedocs.io
 
+
+import sys
 import time
 from networktables import NetworkTables
 
 import logging	# Required
 logging.basicConfig(level=logging.DEBUG)
 
-NetworkTables.initialize()
-table = NetworkTables.getTable("ROS")
+if len(sys.argv) != 2:
+    print("Error: specify an IP to connect to!")
+    exit(0)
 
-alive = 0
+ip = sys.argv[1]
+
+NetworkTables.initialize(server=ip)
+table = NetworkTables.getTable("ML")
 i = 0
 while 1:
-    table.putNumber("alive", alive)
-    print("Sent " + str(alive) + " to networktables")
+    ball1memX = 0
+    object1posX = table.getNumberArray('boxes', [0,0,0,0])
+    numBall = table.getNumber('nb_objects', 0)
+    ball1centerX = ((object1posX[2]-object1posX[0])/2 + object1posX[0])/5.33
+    
+    if (numBall == 1):
+        try:     
+            ball1memX = ball1centerX
+            i = 0
+        except:
+            pass
 
-    alive = alive + 1
-    time.sleep(1)
+    if (i >= 20):
+        ball1memX = 0
+    else:
+        pass
 
-    if i == 10:
-        i = 1
+    if (numBall == 1):
+        try:     
+            print("The center of ball 1 is at " + str(ball1memX))
+        except:
+            print("not enough ballage")
+    elif (numBall == 2):
+        print("The center of ball 1 is at " + str(((object1posX[2]-object1posX[0])/2 + object1posX[0]/5.33)))
+        print("The center of ball 2 is at " + str(((object1posX[3]-object1posX[1])/2 + object1posX[1]/5.33)))
+    elif (numBall > 2):
+        print("Too many balls")
+    else:
+        print("not enough ballage")
+    i = i + 1
+    time.sleep(0.2)
