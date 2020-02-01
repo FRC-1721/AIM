449fd0b18fb54807b47343d99a9d963f1b4ee4fc
KhanSimeoni <simeonikh@gmail.com>
2020-02-01 16:41:39 -0500

    dadawd

--- turn_to_ball.py
+++ turn_to_ball.py
@@ -1,25 +1,34 @@
 #initialize and imports, do not edit
 import sys
 import time
-import frc.robot
+
 from networktables import NetworkTables
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-if len(sys.argv) != 2:
-    print("Error: specify an IP to connect to!")
-    exit(0)
-
-ip = sys.argv[1]
-
-NetworkTables.initialize(server=ip)
+NetworkTables.initialize(server="10.17.21.2")
 table = NetworkTables.getTable("ML")
+NetworkTables.setServer([("10.17.21.2", 5800), ])
 
 #values for tweaking
-
+turnSpeed = 0.2
 #math & code
 while 1:
     b1cX.getNumber('ball1centerX', 0)
     b2cX.getNumber('ball2centerX', 0)
+
+    leftSpeed = 0
+    rightSpeed = 0
+    if (ball1centerX >=25 and ball1centerX <= 35):
+        pass
+    elif(ball1centerX < 25):
+        leftSpeed = turnSpeed
+    elif(ball1centerX > 35):
+        rightSpeed = turnSpeed
+    
+        
+    
+    ros.putNumber("coprocessorPort", leftSpeed)
+    ros.putNumber("coprocessorStarboard", rightSpeed)
 
